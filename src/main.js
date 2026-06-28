7afaca7f1ac964924f1abd516fd1aea59e7f2254
ajay <ajaykundnani03@gmail.com>
2026-06-28 17:51:37 +0530

    All set

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import { createApp } from 'vue'
-import VueGtag from 'vue-gtag-next'
-import App from './App.vue'
+import { createApp } from "vue";
+import VueGtag from "vue-gtag-next";
+import App from "./App.vue";
 
-const app = createApp(App)
+const app = createApp(App);
 
-const gaId = import.meta.env.VITE_GA_MEASUREMENT_ID
+const gaId = import.meta.env.VITE_GA_MEASUREMENT_ID;
 if (gaId) {
   app.use(VueGtag, {
     property: { id: gaId },
-  })
+  });
 }
 
-app.mount('#app')
+app.mount("#app");
